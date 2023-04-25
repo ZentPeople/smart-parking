0ada36f247367b5b043ebf3b49b93cae0308f1f6
Mohan Murugesan <mohan@zentpeople.com>
2023-04-25 21:57:24 +0530

    corrected signin

--- server/routes/users.js
+++ server/routes/users.js
@@ -3,15 +3,10 @@ const router = express.Router();
 const auth = require("../middleware/auth");
 const {
   registerUser,
-  verifyEmail,
   signIn,
   getCurrentUser,
   sendFeedback,
   setProfilePic,
-  sendSubcription,
-  sendResetEmail,
-  resetPassword,
-  resendOTP,
 } = require("../controllers/users");
 
 /*Router indicates the mapping of url string with corresponding handler(controller) function*/
@@ -24,13 +19,8 @@ setting profile pic of a user, a user subscribing to get notifications,
 send email to reset password and resetting password when user forgots password*/
 router.get("", auth, getCurrentUser);
 router.post("/register", registerUser);
-router.post("/resendOTP", resendOTP);
-router.post("/verifyEmail", verifyEmail);
 router.post("/signIn", signIn);
 router.post("/feedback", sendFeedback);
 router.post("/profilePic", auth, setProfilePic);
-router.post("/notifications/subscribe", auth, sendSubcription);
-router.post("/resetEmail", sendResetEmail);
-router.post("/resetPassword", resetPassword);
 
 module.exports = router;
