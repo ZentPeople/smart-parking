7a67f4e9a563cadeb00797d71298f3973b3b7683
Mohan Murugesan <mohan@zentpeople.com>
2023-04-25 21:40:22 +0530

    Completed registration

--- server/models/User.js
+++ server/models/User.js
@@ -1,4 +1,4 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
 /* 
 This schema holds data for user details
@@ -9,58 +9,51 @@ otp while registration,
 role of user or admin
 */
 const UserSchema = mongoose.Schema({
-    firstName:{
-        type:String,
-        required:true,
-        min:2
-    },
-    lastName:{
-        type:String,
-        required:true,
-        min:2
-    },
-    userName:{
-        type:String,
-        required:true,
-        min:5
-    },
-    email:{
-        type:String,
-        required:true,
-    },
-    mobileNo:{
-        type:String,
-        requried:true,
-    },
-    password:{
-        type:String,
-        required:true,
-        min:6
-    },
-    profilePic:{
-        type:String,
-    },
-    verified:{
-        type:Boolean,
-        default:false
-    },
-    subscription:{
-        type:Object,
-    },
-    otp:{
-        type:String,
-        required:true
-    },
-    role:{
-        type:String,
-        enum:["user","admin"],
-        default:"user"
-    },
-    createdAt:{
-        type:Date,
-        default:Date.now()
-    }
-})
+  firstName: {
+    type: String,
+    required: true,
+    min: 2,
+  },
+  lastName: {
+    type: String,
+    required: true,
+    min: 2,
+  },
+  userName: {
+    type: String,
+    required: true,
+    min: 5,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  mobileNo: {
+    type: String,
+    requried: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    min: 6,
+  },
+  profilePic: {
+    type: String,
+  },
+  verified: {
+    type: Boolean,
+    default: false,
+  },
+  role: {
+    type: String,
+    enum: ["user", "admin"],
+    default: "user",
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+});
 
-const User = mongoose.model('User',UserSchema)
-module.exports = User
+const User = mongoose.model("User", UserSchema);
+module.exports = User;
--- server/routes/users.js
+++ server/routes/users.js
@@ -1,7 +1,18 @@
-const express = require('express')
-const router = express.Router()
-const auth = require('../middleware/auth')
-const {sendOTP,verifyEmail,signIn,getCurrentUser, sendFeedback, setProfilePic, sendSubcription, sendResetEmail, resetPassword, resendOTP} = require('../controllers/users')
+const express = require("express");
+const router = express.Router();
+const auth = require("../middleware/auth");
+const {
+  registerUser,
+  verifyEmail,
+  signIn,
+  getCurrentUser,
+  sendFeedback,
+  setProfilePic,
+  sendSubcription,
+  sendResetEmail,
+  resetPassword,
+  resendOTP,
+} = require("../controllers/users");
 
 /*Router indicates the mapping of url string with corresponding handler(controller) function*/
 
@@ -11,15 +22,15 @@ while registration, send an otp, resend an otp, verify a email through otp,
 signinig in a user, a user submitting feedback,
 setting profile pic of a user, a user subscribing to get notifications,
 send email to reset password and resetting password when user forgots password*/
-router.get('',auth,getCurrentUser)
-router.post('/sendOTP',sendOTP)
-router.post('/resendOTP',resendOTP)
-router.post('/verifyEmail',verifyEmail)
-router.post('/signIn',signIn)
-router.post('/feedback',sendFeedback)
-router.post('/profilePic',auth,setProfilePic)
-router.post('/notifications/subscribe',auth,sendSubcription)
-router.post('/resetEmail',sendResetEmail)
-router.post('/resetPassword',resetPassword)
+router.get("", auth, getCurrentUser);
+router.post("/register", registerUser);
+router.post("/resendOTP", resendOTP);
+router.post("/verifyEmail", verifyEmail);
+router.post("/signIn", signIn);
+router.post("/feedback", sendFeedback);
+router.post("/profilePic", auth, setProfilePic);
+router.post("/notifications/subscribe", auth, sendSubcription);
+router.post("/resetEmail", sendResetEmail);
+router.post("/resetPassword", resetPassword);
 
-module.exports = router
+module.exports = router;
